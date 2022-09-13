17854b9003e6a9ef57b1c82d284d3db3c94ca340
Benjamin Figueroa <ben13katcarey@hotmail.com>
2022-09-13 16:06:40 -0500

    commit tree 318d02e8574b09dee8841d62e96ececc4da5d520

---
+++ index.js
@@ -0,0 +1,54 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  "Hello!".toLowerCase(); // 'hello'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    // todo
+  }
+  function logShout(string) {
+    console.log(string);
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  function logWhisper(string) {
+    // todo
+  }
+  function logWhisper(string) {
+    console.log(string);
+  }
+  "Hello!".toLowerCase(); // 'hello'
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  function sayHiToHeadphonedRoommate(aWord) {
+    var cantUnswer = "I can't hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I would love to!";
+    if (aWord.toLowerCase(aWord) === aWord) {
+      return cantUnswer;
+    }
+    else if (aWord.toUpperCase(aWord) === aWord) {
+      return yesUnswer;
+    }
+    else if ("Let's have dinner together!" === aWord) {
+      return lovUnswer
+    }
+  }
